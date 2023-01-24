5ba0bfc1c1b5eda5b1977eab3285e278b2e5dadd
Rron Jakupi <rronjk@gmail.com>
2023-01-25 00:40:13 +0100

    final push, where I totally changed the code since i was not allowed to use a 3rd party library and also handed some edge cases

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
-const Papa = require("papaparse");
+const csv = `EmployeeID,ProjectID,DateFrom,DateTo
+1,1001,2022-01-01,2022-03-31
+1,1002,2022-04-01,2022-05-31
+2,1001,2022-01-01,2022-03-31
+2,1002,2022-04-01,2022-05-31
+3,1001,2022-02-01,2022-03-31
+3,1002,2022-04-01,2022-05-31`;
 
-const findLongestWorkingPair = async (file) => {
-  return await new Promise((resolve, reject) => {
-    Papa.parse(file, {
-      header: true,
-      complete: (results) => {
-        const data = results.data;
-        let workingPairs = {};
-        for (let i = 0; i < data.length; i++) {
-          const empID = data[i].EmpID;
-          const projectID = data[i].ProjectID;
-          const dateFrom = new Date(data[i].DateFrom);
-          const dateTo = new Date(data[i].DateTo);
-          const duration = dateTo.getTime() - dateFrom.getTime();
-          if (workingPairs[projectID]) {
-            for (let j = 0; j < workingPairs[projectID].length; j++) {
-              if (workingPairs[projectID][j].includes(empID)) {
-                workingPairs[projectID][j][2] += duration;
-                break;
-              }
-            }
-          } else {
-            workingPairs[projectID] = [[empID, empID, duration]];
-          }
-        }
-        let longestWorkingPair;
-        let maxDuration = 0;
-        for (let projectID in workingPairs) {
-          for (let i = 0; i < workingPairs[projectID].length; i++) {
-            if (workingPairs[projectID][i][2] > maxDuration) {
-              maxDuration = workingPairs[projectID][i][2];
-              longestWorkingPair = workingPairs[projectID][i];
-            }
-          }
-        }
-        resolve(longestWorkingPair);
-      },
-    });
-  });
+const rows = csv.split('\n').slice(1); // remove header row
+const today = new Date();
+
+let longestPeriod = {
+    employee1: null,
+    employee2: null,
+    project: null,
+    period: 0
 };
+
+rows.forEach(row => {
+    const cells = row.split(',');
+    const employee = cells[0];
+    const project = cells[1];
+    const dateFrom = new Date(cells[2]);
+    let dateTo = cells[3] ? new Date(cells[3]) : today;
+
+    rows.forEach(compareRow => {
+        const compareCells = compareRow.split(',');
+        const compareEmployee = compareCells[0];
+        const compareProject = compareCells[1];
+        const compareDateFrom = new Date(compareCells[2]);
+        let compareDateTo = compareCells[3] ? new Date(compareCells[3]) : today;
+
+        if (employee === compareEmployee || project !== compareProject) {
+            return;
+        }
+
+        const start = dateFrom > compareDateFrom ? dateFrom : compareDateFrom;
+        const end = dateTo < compareDateTo ? dateTo : compareDateTo;
+        const period = end - start;
+
+        if (period > longestPeriod.period) {
+            longestPeriod = {
+                employee1: employee,
+                employee2: compareEmployee,
+                project: project,
+                period: period
+            };
+        }
+    });
+});
+
+const result = [longestPeriod.employee1, longestPeriod.employee2, longestPeriod.project];
+console.log(result);
